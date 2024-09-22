d2ef6ad1f5d8a643b95128ea351392608458335d
Krish Jotaniya <krishjotaniya71@gmail.com>
2024-09-22 17:26:52 +0530

    Update: Admin Credit Purchase and User Subscription Api

--- controllers/Admin/Customer/CreditPurchaseController.js
+++ controllers/Admin/Customer/CreditPurchaseController.js
@@ -1,33 +1,102 @@
 import ApiResponse from '../../../utils/ApiResponse.js';
 import pool from '../../../config/database.js';
-import CustomError from "../../../utils/CustomError.js"
 
 export const getAllCreditPurchaseController = async (req, res, next) => {
+    const connection = await pool.getConnection();
+    const { page = 1, limit = 10, search = '' } = req.query;
     try {
-        const { page = 1, limit = 10, search = '' } = req.query;
-        const limitValue = parseInt(limit, 10);
-        const searchValue = `%${search}%`;
-        const [invoices] = await pool.query(`
-                SELECT c.*, u.firstname, u.lastname, u.email FROM credit_invoice AS c JOIN users AS u ON c.user_id = u.user_id WHERE u.firstname LIKE ? OR u.lastname LIKE ? OR u.email LIKE ? ORDER BY c.createdAt DESC LIMIT ? OFFSET ?
-            `, [searchValue, searchValue, searchValue, limitValue, limitValue * (page - 1)]);
+        let query = `
+            SELECT 
+                i.id, i.customer_name, i.customer_gstno, i.total_price, i.currency, i.invoice_link, i.transaction_id, i.invoice_date,
+                u.user_id, u.firstname, u.lastname, u.email,
+                a.address_name, a.address, a.state, a.city, a.pincode, a.phone_number,
+                ipd.product_description, ipd.amount, ipd.discount, ipd.taxable_value, ipd.cgst_rate, ipd.cgst_amount, ipd.sgst_rate, ipd.sgst_amount, ipd.igst_rate, ipd.igst_amount, ipd.total_amount, ipd.type, i.no_of_credits FROM credit_invoice i
+                JOIN users u ON i.user_id = u.user_id
+                JOIN addresses a ON i.address_id = a.id
+                LEFT JOIN invoice_product_details ipd ON i.id = ipd.invoice_id AND ipd.type = 'credit'
+        `;
 
-        const [[{ totalCount }]] = await pool.query(`SELECT COUNT(*) as totalCount FROM credit_invoice AS c JOIN users AS u ON c.user_id = u.user_id WHERE u.firstname LIKE ? OR u.lastname LIKE ? OR u.email LIKE ?`, [searchValue, searchValue, searchValue]);
-
-        const totalPages = Math.ceil(totalCount / limit);
-
-        if (!invoices.length) {
-            return res.json(new ApiResponse(200, { invoices: [], pagination: { currentPage: page, totalPages, totalItems: totalCount, itemsPerPage: limit } }, 'No invoices found'));
+        const queryParams = [];
+        if (search) {
+            query += ` WHERE u.firstname LIKE ? OR u.lastname LIKE ?`;
+            const searchValue = `%${search}%`;
+            queryParams.push(searchValue, searchValue);
         }
+
+        query += ` LIMIT ? OFFSET ?`;
+        queryParams.push(parseInt(limit), parseInt((page - 1) * limit));
+
+        const invoices = await connection.query(query, queryParams);
+
+        const countQuery = `SELECT COUNT(*) as total FROM credit_invoice i
+                            JOIN users u ON i.user_id = u.user_id
+                            JOIN addresses a ON i.address_id = a.id
+                            LEFT JOIN invoice_product_details ipd ON i.id = ipd.invoice_id AND ipd.type = 'credit'
+                            ${search ? `WHERE u.firstname LIKE ? OR u.lastname LIKE ?` : ''}`;
+        const countParams = search ? [queryParams[0], queryParams[1]] : [];
+        const totalCountResult = await connection.query(countQuery, countParams);
+        const totalCount = totalCountResult[0][0].total;
+
+        const formattedInvoices = invoices[0].map(invoice => {
+            const formattedInvoice = {
+                id: invoice.id,
+                user: {
+                    user_id: invoice.user_id,
+                    firstname: invoice.firstname,
+                    lastname: invoice.lastname,
+                    email: invoice.email
+                },
+                address: {
+                    address_id: invoice.address_id,
+                    address_name: invoice.address_name,
+                    address: invoice.address,
+                    state: invoice.state,
+                    city: invoice.city,
+                    pincode: invoice.pincode,
+                    phone_number: invoice.phone_number,
+                },
+                no_of_credits: invoice.no_of_credits,
+                customer_name: invoice.customer_name,
+                customer_gstno: invoice.customer_gstno,
+                total_price: invoice.total_price,
+                currency: invoice.currency,
+                invoice_link: invoice.invoice_link,
+                transaction_id: invoice.transaction_id,
+                invoice_date: invoice.invoice_date,
+                product_details: {
+                    product_id: invoice.product_id,
+                    invoice_id: invoice.invoice_id,
+                    product_description: invoice.product_description,
+                    amount: invoice.amount,
+                    discount: invoice.discount,
+                    taxable_value: invoice.taxable_value,
+                    cgst_rate: invoice.cgst_rate,
+                    cgst_amount: invoice.cgst_amount,
+                    sgst_rate: invoice.sgst_rate,
+                    sgst_amount: invoice.sgst_amount,
+                    igst_rate: invoice.igst_rate,
+                    igst_amount: invoice.igst_amount,
+                    total_amount: invoice.total_amount
+                },
+                createdAt: invoice.createdAt,
+                updatedAt: invoice.updatedAt,
+            };
+            return formattedInvoice;
+        });
+
         res.json(new ApiResponse(200, {
-            invoices: invoices,
+            invoices: formattedInvoices,
             pagination: {
                 currentPage: Number(page),
-                totalPages,
+                totalPages: Math.ceil(totalCount / limit),
                 totalItems: totalCount,
                 itemsPerPage: Number(limit)
             }
-        }));
+        }, 'Credit Purchase Invoices fetched successfully'));
     } catch (error) {
-        next(new CustomError(500, error.message));
+        console.log(error);
+        res.status(500).json(new ApiResponse(500, 'An error occurred', { error: error.message }));
+    } finally {
+        connection.release();
     }
 };
--- controllers/Admin/Customer/UserSubscriptionController.js
+++ controllers/Admin/Customer/UserSubscriptionController.js
@@ -1,39 +1,97 @@
 import ApiResponse from '../../../utils/ApiResponse.js';
 import pool from '../../../config/database.js';
-import CustomError from "../../../utils/CustomError.js"
 
 export const getAllUserSubscriptionsController = async (req, res, next) => {
+    const connection = await pool.getConnection();
     try {
         const { page = 1, limit = 10 } = req.query;
         const offset = (page - 1) * limit;
-
-        const [[{ totalCount }]] = await pool.query(`SELECT COUNT(*) as totalCount FROM user_subscriptions`);
-        const totalPages = Math.ceil(totalCount / limit);
-
-        const [subscriptions] = await pool.query(`
-            SELECT us.id, us.user_id, us.subscription_id, us.start_date, us.end_date, s.type AS subscription_name, si.total_price, si.currency, si.invoice_link, si.invoice_date, si.transaction_id, si.createdAt, si.updatedAt, u.firstname, u.lastname, u.email
-            FROM user_subscriptions AS us
-            JOIN subscription_invoice AS si ON us.subscription_id = si.subscription_id
-            JOIN subscriptions AS s ON us.subscription_id = s.id
-            JOIN users AS u ON us.user_id = u.user_id
-            ORDER BY si.createdAt DESC
+        let query = `
+        SELECT 
+            i.id, i.customer_name, i.customer_gstno, i.total_price, i.currency, i.invoice_link, i.transaction_id, i.invoice_date,
+            u.user_id, u.firstname, u.lastname, u.email,
+            a.address_name, a.address, a.state, a.city, a.pincode, a.phone_number,
+            ipd.product_description, ipd.amount, ipd.discount, ipd.taxable_value, ipd.cgst_rate, ipd.cgst_amount, ipd.sgst_rate, ipd.sgst_amount, ipd.igst_rate, ipd.igst_amount, ipd.total_amount, ipd.type, s.type, s.credit_amount, s.duration, s.benefits 
+            FROM subscription_invoice i
+            JOIN users u ON i.user_id = u.user_id
+            JOIN addresses a ON i.address_id = a.id
+            LEFT JOIN invoice_product_details ipd ON i.id = ipd.invoice_id AND ipd.type = 'subscription'
+            JOIN subscriptions s ON i.subscription_id = s.id 
+            ORDER BY i.createdAt DESC
             LIMIT ? OFFSET ?
-        `, [parseInt(limit), offset]);
+        `;
 
-        if (!subscriptions.length) {
-            return res.json(new ApiResponse(200, { subscriptions: [], pagination: { currentPage: page, totalPages, totalItems: totalCount, itemsPerPage: limit } }, 'No referrals found'));
-        }
+        const invoices = await connection.query(query, [parseInt(limit), parseInt(offset)]);
+
+        const formattedInvoices = invoices[0].map(invoice => {
+            const formattedInvoice = {
+                id: invoice.id,
+                user: {
+                    user_id: invoice.user_id,
+                    firstname: invoice.firstname,
+                    lastname: invoice.lastname,
+                    email: invoice.email
+                },
+                address: {
+                    address_id: invoice.address_id,
+                    address_name: invoice.address_name,
+                    address: invoice.address,
+                    state: invoice.state,
+                    city: invoice.city,
+                    pincode: invoice.pincode,
+                    phone_number: invoice.phone_number,
+                },
+                customer_name: invoice.customer_name,
+                customer_gstno: invoice.customer_gstno,
+                total_price: invoice.total_price,
+                currency: invoice.currency,
+                invoice_link: invoice.invoice_link,
+                transaction_id: invoice.transaction_id,
+                invoice_date: invoice.invoice_date,
+                product_details: {
+                    product_id: invoice.product_id,
+                    invoice_id: invoice.invoice_id,
+                    product_description: invoice.product_description,
+                    amount: invoice.amount,
+                    discount: invoice.discount,
+                    taxable_value: invoice.taxable_value,
+                    cgst_rate: invoice.cgst_rate,
+                    cgst_amount: invoice.cgst_amount,
+                    sgst_rate: invoice.sgst_rate,
+                    sgst_amount: invoice.sgst_amount,
+                    igst_rate: invoice.igst_rate,
+                    igst_amount: invoice.igst_amount,
+                    total_amount: invoice.total_amount
+                },
+                subscription: {
+                    subscription_id: invoice.subscription_id,
+                    type: invoice.type,
+                    credit_amount: invoice.credit_amount,
+                    duration: invoice.duration,
+                    benefits: invoice.benefits
+                },
+                createdAt: invoice.createdAt,
+                updatedAt: invoice.updatedAt,
+            };
+
+            return formattedInvoice;
+        });
+
+        const totalCount = invoices[0].length;
 
         res.json(new ApiResponse(200, {
-            subscriptions: subscriptions,
+            invoices: formattedInvoices,
             pagination: {
                 currentPage: Number(page),
-                totalPages,
+                totalPages: Math.ceil(totalCount / limit),
                 totalItems: totalCount,
                 itemsPerPage: Number(limit)
             }
         }));
     } catch (error) {
-        next(new CustomError(500, error.message));
+        console.log(error);
+        res.status(500).json(new ApiResponse(500, 'An error occurred', { error: error.message }));
+    } finally {
+        connection.release();
     }
 };

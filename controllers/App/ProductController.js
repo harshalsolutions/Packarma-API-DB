860eb896595f8c16d03299fa16324e762d3208bf
Krish Jotaniya <krishjotaniya71@gmail.com>
2024-10-02 22:37:50 +0530

    Updated: Packaging treatment by subcategory in that cateogyr id added

--- controllers/App/ProductController.js
+++ controllers/App/ProductController.js
@@ -153,7 +153,6 @@ export const getProductsController = async (req, res, next) => {
     }
 };
 
-
 export const searchProductSuggestionsController = async (req, res, next) => {
     try {
         const { query, limit = 10, product } = req.query;
@@ -202,6 +201,7 @@ export const searchProductSuggestionsController = async (req, res, next) => {
         next(new CustomError(500, error.message));
     }
 };
+
 export const getPackingTypesController = async (req, res, next) => {
     try {
         const { status } = req.query;
@@ -260,7 +260,6 @@ export const getShelfLifeOptionsController = async (req, res, next) => {
     }
 };
 
-
 export const getProductWeightOptionsController = async (req, res, next) => {
     try {
         const { product_id } = req.query;
@@ -290,218 +289,6 @@ export const getProductWeightOptionsController = async (req, res, next) => {
         next(new CustomError(500, error.message));
     }
 };
-export const searchPackagingSolutionsController = async (req, res, next) => {
-    const connection = await pool.getConnection();
-    try {
-        await connection.beginTransaction();
-
-        const {
-            category_id,
-            subcategory_id,
-            product_id,
-            packing_type_id,
-            shelf_life_days,
-            product_weight,
-            min_order_quantity_unit_id
-        } = req.body;
-
-        const userId = req.user.userId;
-
-        const ALL_PACKAGING_TYPE_ID = 4;
-
-        let query = `
-        SELECT 
-            ps.*, 
-            c.name AS category_name, c.image AS category_image,
-            p.product_name, p.product_image, p.status AS product_status,
-            pf.name AS product_form_name, pf.image AS product_form_image, pf.short_description AS product_form_description, pf.status AS product_form_status,
-            pt.name AS packaging_treatment_name, pt.image AS packaging_treatment_image, pt.short_description AS packaging_treatment_description, pt.featured AS packaging_treatment_featured, pt.status AS packaging_treatment_status,
-            pk.name AS packing_type_name, pk.short_description AS packing_type_description, pk.status AS packing_type_status,
-            pm.material_name, pm.material_description, pm.wvtr, pm.otr, pm.cof, pm.sit, pm.gsm, pm.special_feature, pm.status AS packaging_material_status,
-            pc.name AS packaging_machine_name, pc.image AS packaging_machine_image, pc.short_description AS packaging_machine_description, pc.status AS packaging_machine_status,
-            mu.name AS min_order_quantity_unit_name, mu.symbol AS min_order_quantity_unit_symbol, mu.status AS min_order_quantity_unit_status,
-            sc.name AS storage_condition_name, sc.short_description AS storage_condition_description, sc.status AS storage_condition_status,
-            s.name AS subcategory_name, s.image AS subcategory_image, s.status AS subcategory_status
-        FROM 
-            packaging_solution ps
-        JOIN 
-            categories c ON ps.product_category_id = c.id
-        JOIN 
-            product p ON ps.product_id = p.id
-        JOIN 
-            subcategories s ON p.sub_category_id = s.id
-        JOIN 
-            product_form pf ON ps.product_form_id = pf.id
-        JOIN 
-            packaging_treatment pt ON ps.packaging_treatment_id = pt.id
-        JOIN 
-            packing_type pk ON ps.packing_type_id = pk.id
-        JOIN 
-            packaging_material pm ON ps.packaging_material_id = pm.id
-        JOIN 
-            packaging_machine pc ON ps.packaging_machine_id = pc.id
-        LEFT JOIN 
-            measurement_unit mu ON ps.min_order_quantity_unit_id = mu.id
-        JOIN 
-            storage_condition sc ON ps.storage_condition_id = sc.id
-        WHERE 
-            ps.status = 'active'
-            AND c.status = 'active'
-            AND p.status = 'active'
-            AND s.status = 'active'
-            AND pf.status = 'active'
-            AND pt.status = 'active'
-            AND pk.status = 'active'
-            AND pm.status = 'active'
-            AND pc.status = 'active'
-            AND sc.status = 'active'
-        `;
-
-        const queryParams = [];
-
-        if (category_id) {
-            query += ' AND ps.product_category_id = ?';
-            queryParams.push(category_id);
-        }
-
-        if (subcategory_id) {
-            query += ' AND p.sub_category_id = ?';
-            queryParams.push(subcategory_id);
-        }
-
-        if (product_id) {
-            query += ' AND ps.product_id = ?';
-            queryParams.push(product_id);
-        }
-
-        if (packing_type_id && packing_type_id !== ALL_PACKAGING_TYPE_ID) {
-            query += ' AND ps.packing_type_id = ?';
-            queryParams.push(packing_type_id);
-        }
-
-        if (min_order_quantity_unit_id) {
-            query += ' AND ps.min_order_quantity_unit_id = ?';
-            queryParams.push(min_order_quantity_unit_id);
-        }
-
-        if (shelf_life_days) {
-            query += ' AND ps.display_shelf_life_days >= ?';
-            queryParams.push(shelf_life_days);
-        }
-
-        if (product_weight) {
-            query += ' AND ps.product_min_weight <= ? AND ps.product_max_weight >= ?';
-            queryParams.push(product_weight, product_weight);
-        }
-
-        query += ' ORDER BY ps.id';
-
-        const [rows] = await connection.query(query, queryParams);
-
-        if (!rows.length) {
-            throw new CustomError(404, 'No packaging solutions found');
-        }
-
-        const packagingSolutionIds = rows.map(row => row.id);
-
-        let deductCredit = true;
-
-        if (packing_type_id === ALL_PACKAGING_TYPE_ID) {
-            const [existingSearchHistory] = await connection.query(`
-                SELECT * FROM search_history 
-                WHERE user_id = ? 
-                AND packing_type_id = ?
-                AND packaging_solution_id IN (?)
-            `, [userId, ALL_PACKAGING_TYPE_ID, packagingSolutionIds]);
-
-            if (existingSearchHistory.length > 0) {
-                deductCredit = false;
-            }
-        } else {
-            const [allTypeSearchHistory] = await connection.query(`
-                SELECT * FROM search_history 
-                WHERE user_id = ?
-                AND packing_type_id = ?
-                AND packaging_solution_id IN (?)
-            `, [userId, ALL_PACKAGING_TYPE_ID, packagingSolutionIds]);
-
-            if (allTypeSearchHistory.length > 0) {
-                deductCredit = false;
-            }
-        }
-
-        if (deductCredit) {
-            const [creditRows] = await connection.query('SELECT credits FROM users WHERE user_id = ?', [userId]);
-            if (!creditRows.length) throw new CustomError(404, 'User not found');
-
-            const currentCredits = creditRows[0].credits;
-
-            if (currentCredits < 1) {
-                throw new CustomError(403, 'Insufficient credits to perform search');
-            }
-
-            const newCredits = currentCredits - 1;
-
-            await connection.query('UPDATE users SET credits = ?, updatedAt = CURRENT_TIMESTAMP WHERE user_id = ?', [newCredits, userId]);
-
-            await connection.query(
-                'INSERT INTO credit_history (user_id, change_amount, description) VALUES (?, ?, ?)',
-                [userId, -1, 'Credit deducted for packaging solution search']
-            );
-        }
-
-        for (const id of packagingSolutionIds) {
-            const [existingHistory] = await connection.query(`
-                SELECT 1 FROM search_history 
-                WHERE user_id = ? 
-                AND packaging_solution_id = ?
-                AND (category_id = ? OR ? IS NULL)
-                AND (subcategory_id = ? OR ? IS NULL)
-                AND (product_id = ? OR ? IS NULL)
-                AND (packing_type_id = ? OR ? IS NULL)
-                AND (shelf_life_days = ? OR ? IS NULL)
-                AND (min_order_quantity_unit_id = ? OR ? IS NULL)
-            `, [
-                userId, id,
-                category_id, category_id,
-                subcategory_id, subcategory_id,
-                product_id, product_id,
-                packing_type_id, packing_type_id,
-                shelf_life_days, shelf_life_days,
-                min_order_quantity_unit_id, min_order_quantity_unit_id
-            ]);
-
-            if (!existingHistory.length) {
-                await connection.query(`
-                    INSERT INTO search_history (user_id, packaging_solution_id, weight_by_user, search_time, category_id, subcategory_id, product_id, packing_type_id, shelf_life_days, min_order_quantity_unit_id)
-                    VALUES (?, ?, ?, CURRENT_TIMESTAMP, ?, ?, ?, ?, ?, ?)
-                `, [
-                    userId,
-                    id,
-                    product_weight || null,
-                    category_id || null,
-                    subcategory_id || null,
-                    product_id || null,
-                    packing_type_id || null,
-                    shelf_life_days || null,
-                    min_order_quantity_unit_id || null
-                ]);
-            }
-        }
-
-        await connection.commit();
-
-        res.json(new ApiResponse(200, rows, 'Packaging solutions fetched successfully'));
-
-    } catch (error) {
-        await connection.rollback();
-        next(new CustomError(500, error.message));
-    } finally {
-        connection.release();
-    }
-};
-
-
 
 export const getSearchHistoryController = async (req, res, next) => {
     const connection = await pool.getConnection();
@@ -571,15 +358,24 @@ export const getSearchHistoryController = async (req, res, next) => {
     }
 };
 
-
 export const getSubCategoryByPackagingTreatmentController = async (req, res, next) => {
     const connection = await pool.getConnection();
     const { id } = req.params;
 
     try {
         const selectQuery = `
-            SELECT DISTINCT s.*
+            SELECT DISTINCT  
+            c.id AS category_id, 
+            c.name AS category_name, 
+            c.image AS category_image, 
+            c.status AS category_status,
+            s.id AS subcategory_id, 
+            s.name AS subcategory_name, 
+            s.image AS subcategory_image, 
+            s.status AS subcategory_status,
+            s.sequence AS subcategory_sequence
             FROM subcategories s
+            JOIN categories c ON s.category_id = c.id
             JOIN product p ON s.id = p.sub_category_id
             WHERE p.packaging_treatment_id = ?
             ORDER BY s.sequence;  
@@ -593,6 +389,222 @@ export const getSubCategoryByPackagingTreatmentController = async (req, res, nex
 
         res.json(new ApiResponse(200, rows, 'Subcategories fetched successfully'));
     } catch (error) {
+        next(new CustomError(500, error.message));
+    } finally {
+        connection.release();
+    }
+};
+
+export const searchPackagingSolutionsController = async (req, res, next) => {
+    const connection = await pool.getConnection();
+    try {
+        await connection.beginTransaction();
+
+        const {
+            category_id,
+            subcategory_id,
+            product_id,
+            packing_type_id,
+            shelf_life_days,
+            product_weight,
+            min_order_quantity_unit_id
+        } = req.body;
+
+        const userId = req.user.userId;
+
+        const ALL_PACKAGING_TYPE_ID = 4;
+
+        let currentPackingTypeId = packing_type_id || ALL_PACKAGING_TYPE_ID;
+
+
+        let query = `
+        SELECT 
+            ps.*, 
+            c.name AS category_name, c.image AS category_image,
+            p.product_name, p.product_image, p.status AS product_status,
+            pf.name AS product_form_name, pf.image AS product_form_image, pf.short_description AS product_form_description, pf.status AS product_form_status,
+            pt.name AS packaging_treatment_name, pt.image AS packaging_treatment_image, pt.short_description AS packaging_treatment_description, pt.featured AS packaging_treatment_featured, pt.status AS packaging_treatment_status,
+            pk.name AS packing_type_name, pk.short_description AS packing_type_description, pk.status AS packing_type_status,
+            pm.material_name, pm.material_description, pm.wvtr, pm.otr, pm.cof, pm.sit, pm.gsm, pm.special_feature, pm.status AS packaging_material_status,
+            pc.name AS packaging_machine_name, pc.image AS packaging_machine_image, pc.short_description AS packaging_machine_description, pc.status AS packaging_machine_status,
+            mu.name AS min_order_quantity_unit_name, mu.symbol AS min_order_quantity_unit_symbol, mu.status AS min_order_quantity_unit_status,
+            sc.name AS storage_condition_name, sc.short_description AS storage_condition_description, sc.status AS storage_condition_status,
+            s.name AS subcategory_name, s.image AS subcategory_image, s.status AS subcategory_status
+        FROM 
+            packaging_solution ps
+        JOIN 
+            categories c ON ps.product_category_id = c.id
+        JOIN 
+            product p ON ps.product_id = p.id
+        JOIN 
+            subcategories s ON p.sub_category_id = s.id
+        JOIN 
+            product_form pf ON ps.product_form_id = pf.id
+        JOIN 
+            packaging_treatment pt ON ps.packaging_treatment_id = pt.id
+        JOIN 
+            packing_type pk ON ps.packing_type_id = pk.id
+        JOIN 
+            packaging_material pm ON ps.packaging_material_id = pm.id
+        JOIN 
+            packaging_machine pc ON ps.packaging_machine_id = pc.id
+        LEFT JOIN 
+            measurement_unit mu ON ps.min_order_quantity_unit_id = mu.id
+        JOIN 
+            storage_condition sc ON ps.storage_condition_id = sc.id
+        WHERE 
+            ps.status = 'active'
+            AND c.status = 'active'
+            AND p.status = 'active'
+            AND s.status = 'active'
+            AND pf.status = 'active'
+            AND pt.status = 'active'
+            AND pk.status = 'active'
+            AND pm.status = 'active'
+            AND pc.status = 'active'
+            AND sc.status = 'active'
+        `;
+
+        const queryParams = [];
+
+        if (category_id) {
+            query += ' AND ps.product_category_id = ?';
+            queryParams.push(category_id);
+        }
+
+        if (subcategory_id) {
+            query += ' AND p.sub_category_id = ?';
+            queryParams.push(subcategory_id);
+        }
+
+        if (product_id) {
+            query += ' AND ps.product_id = ?';
+            queryParams.push(product_id);
+        }
+
+        if (currentPackingTypeId && currentPackingTypeId !== ALL_PACKAGING_TYPE_ID) {
+            query += ' AND ps.packing_type_id = ?';
+            queryParams.push(currentPackingTypeId);
+        }
+
+        if (min_order_quantity_unit_id) {
+            query += ' AND ps.min_order_quantity_unit_id = ?';
+            queryParams.push(min_order_quantity_unit_id);
+        }
+
+        if (shelf_life_days) {
+            query += ' AND ps.display_shelf_life_days >= ?';
+            queryParams.push(shelf_life_days);
+        }
+
+        if (product_weight) {
+            query += ' AND ps.product_min_weight <= ? AND ps.product_max_weight >= ?';
+            queryParams.push(product_weight, product_weight);
+        }
+
+        query += ' ORDER BY ps.id';
+
+        const [rows] = await connection.query(query, queryParams);
+
+        if (!rows.length) {
+            throw new CustomError(404, 'No packaging solutions found');
+        }
+
+        const packagingSolutionIds = rows.map(row => row.id);
+
+        let deductCredit = true;
+
+        if (currentPackingTypeId === ALL_PACKAGING_TYPE_ID) {
+            const [existingSearchHistory] = await connection.query(`
+                SELECT * FROM search_history 
+                WHERE user_id = ? 
+                AND packing_type_id = ?
+                AND packaging_solution_id IN (?)
+            `, [userId, ALL_PACKAGING_TYPE_ID, packagingSolutionIds]);
+
+            if (existingSearchHistory.length > 0) {
+                deductCredit = false;
+            }
+        } else {
+            const [allTypeSearchHistory] = await connection.query(`
+                SELECT * FROM search_history 
+                WHERE user_id = ?
+                AND packing_type_id = ?
+                AND packaging_solution_id IN (?)
+            `, [userId, ALL_PACKAGING_TYPE_ID, packagingSolutionIds]);
+
+            if (allTypeSearchHistory.length > 0) {
+                deductCredit = false;
+            }
+        }
+
+        if (deductCredit) {
+            const [creditRows] = await connection.query('SELECT credits FROM users WHERE user_id = ?', [userId]);
+            if (!creditRows.length) throw new CustomError(404, 'User not found');
+
+            const currentCredits = creditRows[0].credits;
+
+            if (currentCredits < 1) {
+                throw new CustomError(403, 'Insufficient credits to perform search');
+            }
+
+            const newCredits = currentCredits - 1;
+
+            await connection.query('UPDATE users SET credits = ?, updatedAt = CURRENT_TIMESTAMP WHERE user_id = ?', [newCredits, userId]);
+
+            await connection.query(
+                'INSERT INTO credit_history (user_id, change_amount, description) VALUES (?, ?, ?)',
+                [userId, -1, 'Credit deducted for packaging solution search']
+            );
+        }
+
+        for (const id of packagingSolutionIds) {
+            const [existingHistory] = await connection.query(`
+                SELECT 1 FROM search_history 
+                WHERE user_id = ? 
+                AND packaging_solution_id = ?
+                AND (category_id = ? OR ? IS NULL)
+                AND (subcategory_id = ? OR ? IS NULL)
+                AND (product_id = ? OR ? IS NULL)
+                AND (packing_type_id = ? OR ? IS NULL)
+                AND (shelf_life_days = ? OR ? IS NULL)
+                AND (min_order_quantity_unit_id = ? OR ? IS NULL)
+            `, [
+                userId, id,
+                category_id, category_id,
+                subcategory_id, subcategory_id,
+                product_id, product_id,
+                currentPackingTypeId, currentPackingTypeId,
+                shelf_life_days, shelf_life_days,
+                min_order_quantity_unit_id, min_order_quantity_unit_id
+            ]);
+
+            if (!existingHistory.length) {
+                await connection.query(`
+                    INSERT INTO search_history (user_id, packaging_solution_id, weight_by_user, search_time, category_id, subcategory_id, product_id, packing_type_id, shelf_life_days, min_order_quantity_unit_id)
+                    VALUES (?, ?, ?, CURRENT_TIMESTAMP, ?, ?, ?, ?, ?, ?)
+                `, [
+                    userId,
+                    id,
+                    product_weight || null,
+                    category_id || null,
+                    subcategory_id || null,
+                    product_id || null,
+                    currentPackingTypeId || null,
+                    shelf_life_days || null,
+                    min_order_quantity_unit_id || null
+                ]);
+            }
+        }
+
+        await connection.commit();
+        let message = deductCredit ? "Packaging solutions fetched successfully and credit deducted" : "Packaging solutions fetched successfully"
+
+        res.json(new ApiResponse(200, rows, message));
+
+
+    } catch (error) {
+        await connection.rollback();
         next(new CustomError(500, error.message));
     } finally {
         connection.release();
